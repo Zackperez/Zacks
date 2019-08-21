ce2d16a8147075b834c29df42d569e400a64561b
Zacks <jctrujillop@ul.edu.co>
2019-08-21 16:18:58 +0100

    Commit de Prueba

---
+++ PryElectricaribe/src/pryelectricaribe/PryElectricaribe.java
@@ -0,0 +1,21 @@
+
+package pryelectricaribe;
+
+public class PryElectricaribe {
+
+    public static void main(String[] args) {
+     Cobros ven=new Cobros();
+     ven.setVisible(true); 
+     
+     
+     
+  
+        
+        
+        
+        
+        
+        
+    }
+    
+}
